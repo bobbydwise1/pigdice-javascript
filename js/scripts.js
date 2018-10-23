73ea41b4e6540677d5103b21c4164f014f20740c
Charley McGowan and Marie Almquist <charley@>
2018-10-22 19:02:03 -0700

    The game is fully working at this point, except for whoever gets to 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic
 
-var PlayerOne = [];
-var PlayerTwo = [];
+var PlayerOne = 0;
+var PlayerTwo = 0;
 var currentPlayer = 1;
 var currentRoll = 0;
 var currentTurnScore = [];
@@ -25,18 +25,18 @@ var RollTheDice = function(){
 };
 
 var HoldTheDice = function(){
-
+  console.log("This is Hold The Dice working.");
   if (currentPlayer === 1) {
-    PlayerOne.push(currentTurnScore);
+    PlayerOne += currentTurnScore.reduce((a, b) => a + b, 0);
+    console.log("Player One, you've chosen to hold the dice, here's your score: " + PlayerOne);
     currentPlayer += 1;
     currentTurnScore = [];
   } else if (currentPlayer === 2){
-
+    PlayerTwo += currentTurnScore.reduce((a, b) => a + b, 0);
+    console.log("Player Two, you've chosen to hold the dice, here's your score: " + PlayerTwo);
     currentPlayer -= 1;
     currentTurnScore = [];
   }
-
-  return
 };
 
 var playerTurn = function(){
@@ -49,6 +49,16 @@ $(document).ready(function() {
     event.preventDefault();
     RollTheDice();
     $("span#DiceRoll").text("This is what your rolled: " + currentRoll)
-
   });
+  $("form#HoldDice").submit(function(event) {
+    event.preventDefault();
+    HoldTheDice();
+    if (currentPlayer !== 1) {
+      $("span#PlayerOneScore").text(PlayerOne);
+    } else if (currentPlayer !== 2) {
+      $("span#PlayerTwoScore").text(PlayerTwo);
+    }
+
+    $("span#DiceRoll").text("You chose to hold the dice: " + currentRoll)
+  })
 });
