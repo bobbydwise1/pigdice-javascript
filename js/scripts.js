8fb0156b3fd719205284bd4d70bcb0ec985ac8ea
Charley McGowan and Marie Almquist <marie@>
2018-10-22 18:38:11 -0700

    Scores are being added properly, and if a 1 is rolled, it moves to next players turn

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,47 @@
 // Business Logic
+
+var PlayerOne = [];
+var PlayerTwo = [];
+var currentPlayer = 1;
 var currentRoll = 0;
+var currentTurnScore = [];
 
 var RollTheDice = function(){
   currentRoll = Math.floor((Math.random() * 6 + 1 ));
-  console.log("This is the current roll: " + currentRoll);
-  return currentRoll
+  if (currentRoll === 1 && currentPlayer === 1) {
+    currentPlayer += 1;
+    currentTurnScore = [];
+    console.log("Sorry, you rolled a one, next players turn");
+  } else if (currentRoll === 1 && currentPlayer === 2) {
+    currentPlayer -= 1;
+    currentTurnScore = [];
+    console.log("Sorry, you rolled a one, next players turn");
+  } else {
+    currentTurnScore.push(currentRoll);
+    console.log("This is the current turn score: " + currentTurnScore);
+    console.log("This is the current roll: " + currentRoll);
+    return currentRoll
+  }
 };
+
+var HoldTheDice = function(){
+
+  if (currentPlayer === 1) {
+    PlayerOne.push(currentTurnScore);
+    currentPlayer += 1;
+    currentTurnScore = [];
+  } else if (currentPlayer === 2){
+
+    currentPlayer -= 1;
+    currentTurnScore = [];
+  }
+
+  return
+};
+
+var playerTurn = function(){
+
+}
 
 // UI Logic
 $(document).ready(function() {
