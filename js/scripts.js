b18746444f3df09fb95a8ebe0c89bc1af66b9b08
Charley McGowan and Marie Almquist <charley@>
2018-10-24 19:22:57 -0700

    works with objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 // Business Logic
 
-var Player = {
-  playerNumber: 0,
-  playerScore: 0,
+function Player(x) {
+  this.playerName = x;
+  this.playerNumber = 0;
+  this.playerScore = 0;
 }
 
-var gameInstance = {
-  currentPlayer: 1,
-  currentRoll: 0,
-  currentTurnScore: [],
-  addscore: 0
-}
+var player1 = new Player("");
+player1.playerNumber++;
+console.log(player1);
+var player2 = new Player("");
+player2.playerNumber++;
+player2.playerNumber++;
+console.log(player2);
 
 var PlayerOne = 0;
 var PlayerTwo = 0;
@@ -36,12 +38,12 @@ var RollTheDice = function(){
 var HoldTheDice = function(){
   if (currentPlayer === 1) {
     addscore = currentTurnScore.reduce((a, b) => a + b, 0)
-    PlayerOne += addscore;
+    player1.playerScore += addscore;
     currentPlayer += 1;
     currentTurnScore = [];
   } else if (currentPlayer === 2){
     addscore = currentTurnScore.reduce((a, b) => a + b, 0)
-    PlayerTwo += addscore;
+    player2.playerScore += addscore;
     currentPlayer -= 1;
     currentTurnScore = [];
   }
@@ -54,22 +56,32 @@ $(document).ready(function() {
     RollTheDice();
     $("span#DiceRoll").text("This is what your rolled: " + currentRoll)
   });
+
+  $("form#PlayerName").submit(function(event){
+    console.log("test2");
+    event.preventDefault();
+    var inputName1 = $("input#playerName1").val();
+    var inputName2 = $("input#playerName2").val();
+    player1.playerName = inputName1;
+    player2.playerName = inputName2;
+  });
+
   $("form#HoldDice").submit(function(event) {
     event.preventDefault();
     HoldTheDice();
     if (currentPlayer !== 1) {
-      $("span#PlayerOneScore").text(PlayerOne);
+      $("span#PlayerOneScore").text(player1.playerScore);
       $("span#DiceRoll").text("Player One, you've chosen to hold the dice, you added " + addscore + " to your score.")
       addscore = 0;
     } else if (currentPlayer !== 2) {
-      $("span#PlayerTwoScore").text(PlayerTwo);
+      $("span#PlayerTwoScore").text(player2.playerScore);
       $("span#DiceRoll").text("Player Two, you've chosen to hold the dice, you added " + addscore + " to your score.")
       addscore = 0;
     }
 
-    if (PlayerOne >= 100) {
+    if (player1.playerScore >= 100) {
     alert("Player One Wins!")
-    } else if (PlayerTwo >= 100){
+    } else if (player2.playerScore >= 100){
       alert("Player Two Wins!")
     }
   })
